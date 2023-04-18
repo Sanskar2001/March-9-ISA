2173157e4b2d28657656da6bb8f91841078fc077
Sanskar2001 <sanskaratrey22@gmail.com>
2023-04-18 23:39:56 +0530

    lec12

---
+++ Arrays_5/globalVariable.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int x=100;
+int a=120;
+void demo()
+{
+	int b=25;
+	// cout<<b;
+	cout<<x<<endl;
+
+}
+int main()
+{
+	int a=10;
+	cout<<::a;
+
+	// ::->scope resolution operators
+
+
+	// cout<<a<<endl;
+
+	// demo();
+
+	// cout<<x;
+}
---
+++ Arrays_5/macros.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+// macro
+#define pii 3.14
+using namespace std;
+int main()
+{
+	int radius=2;
+	const float pi=3.14;
+
+
+	float area=pii*radius*radius;
+
+	cout<<area;
+
+	/*
+	area=pi*r*r
+	*/
+}
