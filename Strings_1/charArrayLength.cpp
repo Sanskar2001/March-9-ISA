ee4b56a2039ce12b6a8fb8848523588d82e19c50
Sanskar2001 <sanskaratrey22@gmail.com>
2023-04-19 21:50:16 +0530

    lec13

---
+++ Strings_1/charArrayLength.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char str[50];
+
+	cin.getline(str,50,'\n');
+
+	// Calculate the length of this char array 
+
+	
+	int i=0;
+
+
+
+	while(str[i]!='\0')
+	{
+		i++;
+
+	}	
+
+	cout<<"Length of the char array is="<<i;
+
+
+
+}
