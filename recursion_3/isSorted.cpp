c658804fc8b08d9e535d6def8a4f67e3e76ab3c0
Sanskar2001 <sanskaratrey22@gmail.com>
2023-05-04 21:32:08 +0530

    lec18

---
+++ recursion_3/isSorted.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+bool isSorted(int* arr,int currIdx,int n)
+{
+
+	if(currIdx==n-1)
+		return true;
+
+	if(arr[currIdx]>arr[currIdx+1])
+		return false;
+
+	return isSorted(arr,currIdx+1,n);
+}
+int main()
+{
+	int arr[]={1,2,30,4,5};
+	int n=5;
+
+	if(isSorted(arr,0,n))
+		cout<<"Sorted";
+	else
+		cout<<"Unsorted";
+
+}
