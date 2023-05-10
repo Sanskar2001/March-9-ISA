beed3a38d33b61489bb5c57d448c4efe8708c5be
Sanskar2001 <sanskaratrey22@gmail.com>
2023-05-10 21:47:04 +0530

    lec19

---
+++ recursion_4/replacePi.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void replaceWithPi(string str,int pos,string ans)
+{
+
+}
+int main()
+{
+	String str="abpcpidepif";
+	// abpc3.14de3.14f
+
+	replaceWithPi(str,0,"");
+
+
+}
