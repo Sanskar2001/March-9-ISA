041756f2db5312ff12e8a0f13763fdd4bd6a6b5c
Sanskar2001 <sanskaratrey22@gmail.com>
2023-05-10 22:05:43 +0530

    lec19

--- recursion_4/replacePi.cpp
+++ recursion_4/replacePi.cpp
@@ -1,13 +1,32 @@
 #include<iostream>
 using namespace std;
-
 void replaceWithPi(string str,int pos,string ans)
 {
+
+	if(pos>=str.length())
+	{
+		cout<<ans;
+		return;
+	}
+
+	if(str[pos]=='p' && str[pos+1]=='i')
+	{
+		ans+="3.14";
+		pos+=2;
+	}
+	else
+	{
+		ans+=str[pos];
+		pos+=1;
+	}
+	
+
+	replaceWithPi(str,pos,ans);
 
 }
 int main()
 {
-	String str="abpcpidepif";
+	string str="abpcpidepif";
 	// abpc3.14de3.14f
 
 	replaceWithPi(str,0,"");
