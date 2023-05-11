f15c2603d50d8e81b12b3c5f40d1c758b459f427
Sanskar2001 <sanskaratrey22@gmail.com>
2023-05-11 20:57:43 +0530

    lec20

---
+++ recursion_5/permutations.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void permutation(char str[],int pos)
+{
+	if(pos==strlen(str))
+	{
+		cout<<str<<endl;
+		return;
+	}
+
+
+	for(int i=pos;i<strlen(str);i++)
+	{
+		
+		// pos->pos
+		// pos->pos+1
+		// pos->pos+2
+		// pos->pos+3
+		// ....
+		// ....
+		// pos->n-1
+
+		int tmp=str[pos];
+		str[pos]=str[i];
+		str[i]=tmp;
+
+		permutation(str,pos+1);
+	}
+
+}
+
+int main()
+{
+	char str[]="abc";
+	permutation(str,0);
+}
