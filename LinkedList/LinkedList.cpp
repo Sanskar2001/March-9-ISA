eb4e109e143dd5433a80eaa29eef46b80f0c6e30
Sanskar2001 <sanskaratrey22@gmail.com>
2023-06-06 23:13:19 +0530

    lec25

--- LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-
 class Node
 {
 
@@ -18,13 +17,42 @@ class Node
 };
 
 
+void insertionAtHead(Node* head,Node* tail,int data)
+{
+	// Just need info about 3 things
+	// head of LL 
+	// tail of LL
+	// What data needs to be inserted
+
+	if(head==null)
+	{
+		// LL is empty
+		head=new Node(data);
+		tail=head;
+	}
+	else
+	{
+		// LL is non empty
+		Node* newNode=new Node(data);
+		newNode->next=head;
+		head=newNode;
+
+	}
+
+}
+
 
 int main()
 {
-	Node* head=new Node(10);
+	// Node* head=new Node(10);
 
-	cout<<head->data<<"\n";
-	cout<<head->next;
+	// cout<<head->data<<"\n";
+	// cout<<head->next;
+
+	Node* head=NULL;
+	Node* tail=NULL;
+
+
 	
 	return 0;
 }
