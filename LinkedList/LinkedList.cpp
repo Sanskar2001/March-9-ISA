eec52900297a875f89a5b389e79aadd57d42b909
Sanskar2001 <sanskaratrey22@gmail.com>
2023-06-07 23:40:34 +0530

    lec25

--- LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList.cpp
@@ -59,7 +59,11 @@ int lengthOfLL(Node* head)
 {
 	int cnt=0;
 
-
+	while(head!=NULL)
+	{
+		cnt++;
+		head=head->next;
+	}	
 
 
 	return cnt;
@@ -104,6 +108,9 @@ void insertionAtTail(Node* &head,int data)
 */
 
 
+
+
+
 // Optimise Insertion At Tail O(1)
 // If we maintain head and tail pointers both
 // Then we can simply insert a new node at Tail in O(1)
@@ -129,6 +136,89 @@ void insertionAtTail(Node* &head,Node* &tail,int data)
 
 
 
+
+void insertionAtKthPosition(Node* &head,Node* &tail,int data,int k)
+{
+
+
+	if(k==0)
+	{
+		insertionAtHead(head,tail,data);
+		return;
+	}
+
+	else if(k>=lengthOfLL(head))
+	{
+		insertionAtTail(head,tail,data);
+		return;
+	}
+
+	else
+	{
+
+		// take k-1 jumps from head
+
+		Node* curr=head;
+
+		for(int i=1;i<=k-1;i++)
+		curr=curr->next;
+
+		Node* newNode=new Node(data);
+		Node* tmp=curr->next;
+
+		curr->next=newNode;
+		newNode->next=tmp;
+
+	}
+
+
+}
+
+
+void deleteAtHead(Node* &head,Node* &tail)
+{
+	// LL IS EMPTY
+	if(head==NULL)
+	{
+		cout<<"Linked List is empty can't delete!!";
+		return;
+	}
+	// IF A SINGLE NODE IS THERE IN LL
+	else if(head->next==NULL)
+	{
+		delete head;
+		head=NULL;
+		tail=NULL;
+	}
+	else
+	{
+		Node* tmp=head;
+		head=head->next;
+		delete tmp;
+	}
+
+}
+
+void deleteAtTail(Node* &head,Node* &tail)
+{
+	// LL is empty
+	if(head==NULL)
+	{
+
+	}
+	// If a single node is there in LL
+	else if(head->next==NULL)
+	{
+
+	}
+	// Otherwise if there are many nodes in LL
+	else
+	{
+			
+	}
+}
+
+
 int main()
 {
 	// Node* head=new Node(10);
@@ -150,6 +240,20 @@ int main()
 	insertionAtTail(head,tail,30); // 10->20->30->NULL
 
 	display(head);
+	cout<<"\n";
+
+	insertionAtKthPosition(head,tail,100,1);
+
+	display(head);
+
+	deleteAtHead(head,tail);
+	cout<<"\n";
+	display(head);
+
+	deleteAtHead(head,tail);
+	cout<<"\n";
+	display(head);
+	
 
 
 	
