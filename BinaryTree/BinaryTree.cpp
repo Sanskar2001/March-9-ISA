909145852ce14d3b089a76dc01c025e75b010d7a
Sanskar2001 <sanskaratrey22@gmail.com>
2023-06-27 21:23:05 +0530

    lec33

---
+++ BinaryTree/BinaryTree.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+class node
+{
+	public:
+	int data;
+	node* left;
+	node* right;
+	node(int val)
+	{
+		data=val;
+		left=NULL;
+		right=NULL;
+	}
+};
+
+node* buildTree()
+{
+
+	int d;
+	cin>>d;
+
+	if(d==-1)
+	return NULL;
+
+	node* root=new node(d);
+	root->left=buildTree();
+	root->right=buildTree();
+}
+
+int main()
+{
+	node* root=new node(100);
+	cout<<root->data<<endl;
+	cout<<root->left<<endl;
+	cout<<root->right<<endl;
+}
